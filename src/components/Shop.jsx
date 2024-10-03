121ca50261e5f104fabdba16abb11de6c97d25d8
Akshat <raghuvanshiakshatsingh@gmail.com>
2024-10-03 16:58:57 +0530

    shopping cart

---
+++ src/components/Shop.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import list from '../list';
+import Card from './Card';
+
+const Shop = ({handleClick}) => {
+  return (
+    <div>
+       <section>
+        {list.map((item)=>{
+           return <Card item={item} key={item.id}
+            handleClick={handleClick}/>
+            
+        })}
+       </section>
+    </div>
+  )
+}
+
+export default Shop
